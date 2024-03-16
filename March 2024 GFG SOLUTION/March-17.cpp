cfe3ca53cdfe4ea11025b2b9e0d6b21f3df1f7a8
Patel HetKumar SandipBhai <124852522+Hunterdii@users.noreply.github.com>
2024-03-17 00:45:22 +0530

    Create March-17.cpp
    Solved GFG POTD MARCH17

---
+++ March 2024 GFG SOLUTION/March-17.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+        unordered_set<int> set1, set2;
+        int count = 0;
+        
+        // Populate set1 with the elements of the first linked list
+        while (head1 != NULL) {
+            set1.insert(head1->data);
+            head1 = head1->next;
+        }
+        
+        // Traverse the second linked list and check for complements
+        while (head2 != NULL) {
+            int complement = x - head2->data;
+            if (set1.find(complement) != set1.end()) {
+                count++;
+            }
+            head2 = head2->next;
+        }
+        
+        return count;
+    }
+};
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
