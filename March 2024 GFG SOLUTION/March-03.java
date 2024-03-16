f3af98c25ddf5a5809f02f5e3206614baa7240af
Patel HetKumar SandipBhai <124852522+Hunterdii@users.noreply.github.com>
2024-03-16 05:33:54 +0530

    Create March-03.java

---
+++ March 2024 GFG SOLUTION/March-03.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] arr = br.readLine().trim().split(" ");
+
+            String ans = new Solution().printLargest(n, arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String printLargest(int n, String[] arr) {
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+
+        StringBuilder ans = new StringBuilder();
+        for (String s : arr) {
+            ans.append(s);
+        }
+        return ans.toString();
+    }
+}
