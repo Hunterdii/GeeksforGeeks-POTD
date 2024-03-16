f49a64fb1db96b34d4930a5a326988a92b456986
Patel HetKumar SandipBhai <124852522+Hunterdii@users.noreply.github.com>
2024-03-17 00:46:03 +0530

    Create March-17.java
    Solved GFG POTD MARCH17 JAVA

---
+++ March 2024 GFG SOLUTION/March-17.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class count_pairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n1 = sc.nextInt();
+            Integer arr1[] = new Integer[n1];
+
+            for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
+
+            int n2 = sc.nextInt();
+            Integer arr2[] = new Integer[n2];
+
+            for (int i = 0; i < n2; i++) arr2[i] = sc.nextInt();
+
+            LinkedList<Integer> head1 = new LinkedList<>(Arrays.asList(arr1));
+            LinkedList<Integer> head2 = new LinkedList<>(Arrays.asList(arr2));
+
+            int x = sc.nextInt();
+            Solution gfg = new Solution();
+            System.out.println(gfg.countPairs(head1, head2, x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// your task is to complete this function
+
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2, int x) {
+        HashSet<Integer> set1 = new HashSet<>();
+        int count = 0;
+
+        // Populate set1 with the elements of the first linked list
+        for (int value : head1) {
+            set1.add(value);
+        }
+
+        // Traverse the second linked list and check for complements
+        for (int value : head2) {
+            int complement = x - value;
+            if (set1.contains(complement)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
