ee8bb5fc37ef3d49474a432366a138601ac4f036
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-03-24 16:57:26 +0530

    Update March-24.py

--- March 2024 GFG SOLUTION/March-24.py
+++ March 2024 GFG SOLUTION/March-24.py
@@ -1,17 +1,13 @@
 #User function Template for python3
-#User function Template for python3
 class Solution:
     def insertAtBottom(self, st, x):
         tmp = []
         
-        # Push all elements from the original stack to a temporary list
         while st:
             tmp.append(st.pop())
         
-        # Push the new element to the bottom of the stack
         st.append(x)
         
-        # Push back all elements from the temporary list to the original stack
         while tmp:
             st.append(tmp.pop())
         
