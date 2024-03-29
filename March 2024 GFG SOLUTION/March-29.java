45c1d315c32728b2b0e6b7089b8364c0eec103f5
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-03-29 08:28:43 +0530

    Create March-29.java
    JAVA POTD GFG Solved March29

---
+++ March 2024 GFG SOLUTION/March-29.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isEularCircuitExist(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
+        for (ArrayList<Integer> neighbors : adj) {
+            if (neighbors.size() % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
