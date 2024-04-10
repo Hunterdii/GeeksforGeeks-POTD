ae8327411b93fb0b32c4822fb4650e9066533f8c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-04-10 12:21:46 +0530

    Create April-10.cpp
    Solved GFG POTD

---
+++ April 2024 GFG SOLUTION/April-10.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int findSingle(int n, int arr[]){
+        int single = 0;
+        
+        for(int i = 0; i < n; i++){
+            single ^= arr[i];
+        }
+        
+        return single;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
