5ffbd510e652fff59a3e8ccd749f56ff84d5e10c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-04-19 09:31:05 +0530

    Create April-19.java
    Java Solved GFG POTD

---
+++ April 2024 GFG SOLUTION/April-19.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+class Solution {
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m) {
+        HashSet<Integer> s = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; ++i) {
+            s.add(b[i]);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (!s.contains(a[i])) {
+                ans.add(a[i]);
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int m =Integer.parseInt(q[1]);
+            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(a1[i]);
+            }
+            String line2 = br.readLine();
+            String[] a2 = line2.trim().split("\\s+");
+            int b[] = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(a2[i]);
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans=ob.findMissing(a,b,n,m);
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print(ans.get(i)+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
