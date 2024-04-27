c3dfa9a06b1cbb6f9c591027a741f0f54973ffa9
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-04-27 08:46:43 +0530

    Create April-27.java
    JAVA GFG POTD Solved

---
+++ April 2024 GFG SOLUTION/April-27.java
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int key) {
+        data = key;
+        next = prev = null;
+    }
+}
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                Node n1 = new Node(a);
+                n1.prev = temp;
+                temp.next = n1;
+                temp = n1;
+            }
+
+            head = new Solution().sortDoubly(head);
+            printList(head);
+        }
+    }
+
+    public static void printList(Node node) {
+        Node temp = node;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            temp = node;
+            node = node.next;
+        }
+        System.out.println();
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort the given doubly linked list using Merge Sort.
+    static Node sortDoubly(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node mid = getMid(head);
+        Node nextToMid = mid.next;
+        mid.next = null;
+        nextToMid.prev = null;
+
+        Node left = sortDoubly(head);
+        Node right = sortDoubly(nextToMid);
+
+        return merge(left, right);
+    }
+
+    static Node merge(Node left, Node right) {
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        Node result;
+        if (left.data <= right.data) {
+            result = left;
+            result.next = merge(left.next, right);
+            if (result.next != null) {
+                result.next.prev = result;
+            }
+        } else {
+            result = right;
+            result.next = merge(left, right.next);
+            if (result.next != null) {
+                result.next.prev = result;
+            }
+        }
+
+        return result;
+    }
+
+    static Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
