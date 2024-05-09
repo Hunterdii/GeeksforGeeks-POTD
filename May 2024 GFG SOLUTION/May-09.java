3c1842f951ec2079439dca4a8aa413a95c59e56a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-05-09 13:02:48 +0530

    Create May-09.java
    JAVA GFG POTD Solved

---
+++ May 2024 GFG SOLUTION/May-09.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            String ans = obj.divisorGame(n) ? "True" : "False";
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        return n%2==0;    
+        
+    }
+}
