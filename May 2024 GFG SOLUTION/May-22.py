55ef000369cc553ea81595755a9e89184bcba5db
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-05-25 17:29:54 +0530

    Update May-22.py

--- May 2024 GFG SOLUTION/May-22.py
+++ May 2024 GFG SOLUTION/May-22.py
@@ -16,10 +16,12 @@ class Solution:
 
     def findSmallestMaxDist(self, stations, K):
         stations.sort()
-        l, h = 1e-9, stations[-1] - stations[0]
-        
-        while h - l > 1e-6:
-            mid = (l + h) / 2.0
+        n = len(stations)
+        l = 1e-9
+        h = stations[-1] - stations[0]
+
+        while (h - l) > 1e-6:
+            mid = l + (h - l) / 2.0
             intervals = self.countIntervals(mid, stations)
             if intervals > K:
                 l = mid
