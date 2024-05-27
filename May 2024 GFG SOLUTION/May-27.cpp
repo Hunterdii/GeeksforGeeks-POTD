d1d1b8a1c41c85bc1ccc917149d978e437995923
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-05-27 10:14:51 +0530

    Update May-27.cpp

--- May 2024 GFG SOLUTION/May-27.cpp
+++ May 2024 GFG SOLUTION/May-27.cpp
@@ -22,29 +22,6 @@ class Array {
 
 
 // } Driver Code Ends
-
-// class Solution {
-//   public:
-//     int longestSubseq(int n, vector<int> &a) {
-//         // code here
-//         vector<int>dp(n,0);
-
-//         int ans=0;
-//         for(int i=n-1;i>=0;i--){
-//             int maxi=0;
-//             for(int j=i+1;j<n;j++){
-//                 if(abs(a[i]-a[j])==1){
-//                     maxi=max(maxi, dp[j]);
-//                 }
-//             }
-//             dp[i]=1+maxi;
-//             ans=max(ans, dp[i]);
-//         }
-//         return ans;
-
-//     }
-// };
-
 class Solution {
 public:
     int longestSubseq(int n, vector<int> &a) {
