8369ce2710f3a591ad741849bbcbeeb64c81fe2f
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-05-31 10:32:34 +0530

    Create May-31.java
    Java GFG POTD Solved

---
+++ May 2024 GFG SOLUTION/May-31.java
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.swapNibbles(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int swapNibbles(int n) {
+      int rn = (n & 0xF0) >> 4; 
+        int ln = (n & 0x0F) << 4; 
+        return rn | ln;
+    }
+}
