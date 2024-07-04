5ff4162c247b4a7ba410ba398f4b7fa60f7a73dc
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-07-04 10:38:04 +0530

    Create July-04.java
    Java GFG POTD Solved

---
+++ July 2024 GFG SOLUTION/July-04.java
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) { this.data = data; }
+}
+
+class GFG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void preorder(Node root, List<Integer> temp) {
+        if (root == null) return;
+        temp.add(root.data);
+        preorder(root.left, temp);
+        preorder(root.right, temp);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.next());
+        sc.nextLine();
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            Node root = buildTree(s);
+            Solution obj = new Solution();
+            List<Node> res = obj.printAllDups(root);
+
+            List<List<Integer>> ans = new ArrayList<>();
+
+            for (int i = 0; i < res.size(); i++) {
+                List<Integer> temp = new ArrayList<>();
+                preorder(res.get(i), temp);
+                ans.add(temp);
+            }
+            Collections.sort(ans, (x, y) -> x.get(0) - y.get(0));
+
+            for (int i = 0; i < ans.size(); i++) {
+                for (int j = 0; j < ans.get(i).size(); j++) {
+                    System.out.print(ans.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    private String helper(Node root, List<Node> ans, Map<String, Integer> m) {
+        if (root == null) {
+            return "#";
+        }
+        String left = helper(root.left, ans, m);
+        String right = helper(root.right, ans, m);
+        String node = root.data + "," + left + "," + right;
+
+        if (m.getOrDefault(node, 0) == 1) {
+            ans.add(root);
+        }
+        m.put(node, m.getOrDefault(node, 0) + 1);
+        return node;
+    }
+
+    public List<Node> printAllDups(Node root) {
+        List<Node> ans = new ArrayList<>();
+        Map<String, Integer> m = new HashMap<>();
+        helper(root, ans, m);
+        return ans;
+    }
+}
