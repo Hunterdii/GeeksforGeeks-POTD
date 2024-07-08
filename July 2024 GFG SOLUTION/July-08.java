462dda063f4eaf68286020277025725f47e664db
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-07-08 07:31:24 +0530

    Create July-08.java
    Java GFG POTD Solved

---
+++ July 2024 GFG SOLUTION/July-08.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+
+            out.println(new Solution().search(arr, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int search(int[] arr, int key) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
