9cdc8aefbbdd2bcf1cf41ecd03ac1c79d3257050
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-07-31 16:06:16 +0530

    Create July-31.java

---
+++ July 2024 GFG SOLUTION/July-31.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String arr[] = read.readLine().trim().split(" ");
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonPrefix(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String longestCommonPrefix(String arr[]) {
+        if (arr.length == 0) return "-1";
+        String prefix = arr[0];
+
+        for (int i = 1; i < arr.length; ++i) {
+            while (arr[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "-1";
+            }
+        }
+        return prefix.isEmpty() ? "-1" : prefix;
+    }
+}
