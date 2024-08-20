a7d42e1283bd4168f26958a48726e317f4febd7b
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-08-20 13:47:16 +0530

    Create August-20.cpp

---
+++ August 2024 GFG SOLUTION/August-20.cpp
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* n) {
+        if (!n) return 0;
+        return 1 + std::max(maxDepth(n->left), maxDepth(n->right));
+    }
+
+    int traverse(Node* n, int target, int& ret) {
+        if (!n) return 0;
+        if (n->data == target) {
+            ret = std::max(ret, maxDepth(n->right));
+            ret = std::max(ret, maxDepth(n->left));
+            return 1;
+        }
+        
+        int left = traverse(n->left, target, ret);
+        int right = traverse(n->right, target, ret);
+
+        if (left > 0) {
+            ret = std::max(ret, left + maxDepth(n->right));
+            return left + 1;
+        }
+
+        if (right > 0) {
+            ret = std::max(ret, right + maxDepth(n->left));
+            return right + 1;
+        }
+
+        return 0;
+    }
+
+    int minTime(Node* root, int target) {
+        int ret = 0;
+        traverse(root, target, ret);
+        return ret;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
