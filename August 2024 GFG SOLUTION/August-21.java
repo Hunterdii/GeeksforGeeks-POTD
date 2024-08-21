23e5da9a6331246dfda557f97ab500e5ba0cb6d0
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-08-21 17:57:59 +0530

    Create August-21.java

---
+++ August 2024 GFG SOLUTION/August-21.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+class Solution {
+    public int[] shortestPath(int[][] edges, int n, int m, int src) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<Integer> q = new LinkedList<>();
+        dist[src] = 0;
+        q.add(src);
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int neighbor : adj.get(node)) {
+                if (dist[node] + 1 < dist[neighbor]) {
+                    dist[neighbor] = dist[node] + 1;
+                    q.add(neighbor);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) dist[i] = -1;
+        }
+
+        return dist;
+    }
+}
