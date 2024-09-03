1d503ce8811e11d0b66b4801878e269fc27f6d87
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-09-03 16:34:30 +0530

    Create September-03.java

---
+++ September 2024 GFG SOLUTION/September-03.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int minOperations(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[j] = prev + 1;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                prev = temp;
+            }
+        }
+
+        return m + n - 2 * dp[n];
+    }
+}
