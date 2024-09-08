2b68634b6c4bb3d4d3151af8636f239bcbf9506c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-09-08 18:34:26 +0530

    Create September-08.java

---
+++ September 2024 GFG SOLUTION/September-08.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            System.out.println(new Solution().minJumps(arr));
+            // System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    static int minJumps(int[] arr) {
+        int n = arr.length;
+        if (n <= 1) return 0;
+        if (arr[0] == 0) return -1;
+        
+        int maxReach = arr[0];
+        int steps = arr[0];
+        int jumps = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (i == n - 1) return jumps;
+
+            maxReach = Math.max(maxReach, i + arr[i]);
+            steps--;
+
+            if (steps == 0) {
+                jumps++;
+                if (i >= maxReach) return -1;
+
+                steps = maxReach - i;
+            }
+        }
+
+        return -1;
+    }
+}
