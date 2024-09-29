ba7a9aeb453f41c0b8d77af6a7fb9d0178bae01a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-09-29 14:42:56 +0530

    Create September-29.java

---
+++ September 2024 GFG SOLUTION/September-29.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    int totalCount(int k, int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            count += (num + k - 1) / k;  
+        }
+        return count;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.totalCount(k, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
