eec2adc16a3bb2afeca1c268715fdf7c391e686d
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-09-30 09:57:10 +0530

    Create September-30.java

---
+++ September 2024 GFG SOLUTION/September-30.java
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+
+            s = br.readLine();
+            Node root2 = buildTree(s);
+
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1, root2);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+
+*/
+class Solution {
+    private void pushLeft(Node root, Stack<Node> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    public List<Integer> merge(Node root1, Node root2) {
+        List<Integer> result = new ArrayList<>();
+        Stack<Node> s1 = new Stack<>();
+        Stack<Node> s2 = new Stack<>();
+        pushLeft(root1, s1);
+        pushLeft(root2, s2);
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            if (s2.isEmpty() || (!s1.isEmpty() && s1.peek().data <= s2.peek().data)) {
+                Node node = s1.pop();
+                result.add(node.data);
+                pushLeft(node.right, s1);
+            } else {
+                Node node = s2.pop();
+                result.add(node.data);
+                pushLeft(node.right, s2);
+            }
+        }
+
+        return result;
+    }
+}
