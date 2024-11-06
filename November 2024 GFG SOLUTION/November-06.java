b7364062a2338808e323635aeafe94602bfb989b
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-11-06 17:09:48 +0530

    Create November-06.java

---
+++ November 2024 GFG SOLUTION/November-06.java
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+// Contributed by Sudarshan Sharma
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution ob = new Solution();
+            System.out.println(ob.treePathsSum(root));
+            // System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below.
+Node is as follows:
+class Tree
+{
+      int data;
+      Tree left,right;
+      Tree(int d){
+          data=d;
+          left=null;
+          right=null;
+}
+}*/
+class Solution {
+    public int treePathsSum(Node root) {
+        return calculateSum(root, 0);
+    }
+
+    private int calculateSum(Node node, int currentValue) {
+        if (node == null) return 0;
+        currentValue = currentValue * 10 + node.data;
+        if (node.left == null && node.right == null) {
+            return currentValue;
+        }
+        return calculateSum(node.left, currentValue) + calculateSum(node.right, currentValue);
+    }
+}
