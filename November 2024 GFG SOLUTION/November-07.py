384b750071b6934b628aada01549799e66ebc9b6
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-11-07 20:59:16 +0530

    Update November-07.py

--- November 2024 GFG SOLUTION/November-07.py
+++ November 2024 GFG SOLUTION/November-07.py
@@ -1,27 +1,30 @@
 #User function Template for python3
 class Solution:
+    
     def findSplit(self, arr):
-        n = len(arr)
-        totalSum = sum(arr)
-
-        if totalSum % 3 != 0:
+        total_sum = sum(arr)
+        if total_sum % 3 != 0:
             return [-1, -1]
 
-        target = totalSum // 3
-        currentSum = 0
-        countFirst = 0
+        target_sum = total_sum // 3
+        current_sum = 0
+        first_index, second_index = -1, -1
 
-        for i in range(n):
-            currentSum += arr[i]
+        for i in range(len(arr)):
+            current_sum += arr[i]
 
-            if currentSum == 2 * target and countFirst > 0:
-                return [countFirst - 1, i]
+            if current_sum == target_sum and first_index == -1:
+                first_index = i
+            elif current_sum == 2 * target_sum and first_index != -1:
+                second_index = i
+                break
 
-            if currentSum == target:
-                countFirst += 1
+        if first_index != -1 and second_index != -1:
+            last_part_sum = sum(arr[second_index + 1:])
+            if last_part_sum == target_sum:
+                return [first_index, second_index]
 
         return [-1, -1]
-
 #{ 
  # Driver Code Starts
 # Initial Template for Python 3
@@ -36,10 +39,22 @@ if __name__ == '__main__':
         ob = Solution()
         result = ob.findSplit(arr)
 
-        if result == [-1, -1]:
+        if (result == [-1, -1]) or len(result) != 2:
             print("false")
         else:
-            print("true")
+            sum1 = sum2 = sum3 = 0
+            for i in range(len(arr)):
+                if i <= result[0]:
+                    sum1 += arr[i]
+                elif i <= result[1]:
+                    sum2 += arr[i]
+                else:
+                    sum3 += arr[i]
+
+            if sum1 == sum2 == sum3:
+                print("true")
+            else:
+                print("false")
         print("~")
 
 # } Driver Code Ends
