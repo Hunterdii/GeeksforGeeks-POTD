bf415106fb1203db32b3011fb71d290516e94fbd
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-11-21 14:05:28 +0530

    Create November-21.py

---
+++ November 2024 GFG SOLUTION/November-21.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def maximumProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        solution = Solution()
+        res = solution.maximumProfit(arr)
+        print(res)
+
+# } Driver Code Ends
