fa181dd283556da760c62d41f7203182a0891cd3
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-11-30 15:34:13 +0530

    Create November-30.java

---
+++ November 2024 GFG SOLUTION/November-30.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] counts = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            counts[s1.charAt(i) - 'a']++;
+            counts[s2.charAt(i) - 'a']--;
+        }
+
+        for (int count : counts) {
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+}
