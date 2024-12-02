442d039a3b79e6c64eff45d17b4053bd6796358a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-02 11:57:56 +0530

    Create December-02.java

---
+++ December 2024 GFG SOLUTION/December-02.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+
+            Solution ob = new Solution();
+
+            ArrayList<Integer> res = ob.search(patt, s);
+            if (res.size() == 0)
+                System.out.print("[]");
+            else {
+                for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    private void computeLPSArray(String pat, int m, int[] lps) {
+        int len = 0;
+        int i = 1;
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int m = pat.length();
+        int n = txt.length();
+        int[] lps = new int[m];
+
+        computeLPSArray(pat, m, lps);
+
+        int i = 0, j = 0;
+        while (i < n) {
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+            }
+
+            if (j == m) {
+                result.add(i - j);
+                j = lps[j - 1];
+            } else if (i < n && txt.charAt(i) != pat.charAt(j)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+}
