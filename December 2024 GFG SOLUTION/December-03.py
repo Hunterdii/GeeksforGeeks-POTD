1c74428693890e6fea1d583b61e6926e2eb32aae
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-03 11:42:45 +0530

    Create December-03.py

---
+++ December 2024 GFG SOLUTION/December-03.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minChar(self, s):
+        n = len(s)
+
+        rev_str = s[::-1]
+
+        combined = s + "$" + rev_str
+
+        lps = [0] * len(combined)
+        for i in range(1, len(combined)):
+            j = lps[i - 1]
+            while j > 0 and combined[i] != combined[j]:
+                j = lps[j - 1]
+            if combined[i] == combined[j]:
+                j += 1
+            lps[i] = j
+
+        return n - lps[-1]
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        s = input()
+        obj = Solution()
+        ans = obj.minChar(s)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
