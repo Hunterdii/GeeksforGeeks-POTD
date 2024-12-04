555a3b35ba28c3d88e228292f810dc2960e00717
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-04 20:08:03 +0530

    Create December-04.java

---
+++ December 2024 GFG SOLUTION/December-04.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        // taking input using BufferedReader class
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // taking total count of testcases
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // Reading the two Strings
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+
+            // Creating an object of class Rotate
+            Solution obj = new Solution();
+
+            // calling areRotations method
+            // of class Rotate and printing
+            //"1" if it returns true
+            // else "0"
+            if (obj.areRotations(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public static boolean areRotations(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String temp = s1 + s1;
+        return kmpSearch(temp, s2);
+    }
+
+    private static boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPSArray(pattern); 
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == pattern.length()) {
+                    return true; 
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+    private static int[] computeLPSArray(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0, i = 1;
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
+
