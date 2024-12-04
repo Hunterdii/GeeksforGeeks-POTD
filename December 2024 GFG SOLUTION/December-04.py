9c84ebee5bd94eadde21814b773ebf0e95f52900
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-04 20:08:29 +0530

    Create December-04.py

---
+++ December 2024 GFG SOLUTION/December-04.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+class Solution:
+    def areRotations(self, s1, s2):
+        if len(s1) != len(s2):
+            return False
+        temp = s1 + s1
+        return s2 in temp#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s1 = str(input())
+        s2 = str(input())
+        if (Solution().areRotations(s1, s2)):
+            print("true")
+        else:
+            print("false")
+
+# } Driver Code Ends
