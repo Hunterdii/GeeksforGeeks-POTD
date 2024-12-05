fb45f8e4937d3cef3a30e6e1dd2422a9f61e9a6a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-05 08:15:42 +0530

    Create December-05.py

---
+++ December 2024 GFG SOLUTION/December-05.py
@@ -0,0 +1,42 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+
+# User function Template for Python
+class Solution:
+    def sort012(self, arr):
+        low, mid, high = 0, 0, len(arr) - 1
+
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+        
+
+#{ 
+ # Driver Code Starts.
+def main():
+    t = int(input().strip())  # Read the number of test cases
+    ob = Solution()
+
+    while t > 0:
+        t -= 1
+        arr = list(map(int,
+                       input().strip().split())
+                   )  # Read the array as space-separated integers
+        ob.sort012(arr)  # Sort the array
+
+        print(' '.join(map(str, arr)))  # Print the sorted array
+        print("~")
+        
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
