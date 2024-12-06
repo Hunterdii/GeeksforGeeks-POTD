bae5a23d62a5e865803fc3ffb67eca828a20083d
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-06 07:51:51 +0530

    Create December-06.java

---
+++ December 2024 GFG SOLUTION/December-06.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test_cases = Integer.parseInt(br.readLine().trim());
+
+        while (test_cases-- > 0) {
+            // Read the array from input line
+            String[] input = br.readLine().trim().split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+
+            // Solution instance to invoke the function
+            Solution ob = new Solution();
+            int result = ob.hIndex(arr);
+
+            System.out.println(result);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+
+        for (int c : citations) {
+            if (c >= n)
+                buckets[n]++;
+            else
+                buckets[c]++;
+        }
+
+        int cumulative = 0;
+        for (int i = n; i >= 0; i--) {
+            cumulative += buckets[i];
+            if (cumulative >= i)
+                return i;
+        }
+        return 0;
+    }
+}
