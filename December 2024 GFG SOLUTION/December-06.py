e6dc068cf50d72b409f6e15b17137a7b816e5aa8
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-06 07:54:25 +0530

    Create December-06.py

---
+++ December 2024 GFG SOLUTION/December-06.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+class Solution:
+    def hIndex(self, citations):
+        n = len(citations)
+        buckets = [0] * (n + 1)
+
+        for c in citations:
+            if c >= n:
+                buckets[n] += 1
+            else:
+                buckets[c] += 1
+
+        cumulative = 0
+        for i in range(n, -1, -1):
+            cumulative += buckets[i]
+            if cumulative >= i:
+                return i
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+# Main
+if __name__ == '__main__':
+    t = int(input())
+    while t > 0:
+        t -= 1
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+        result = ob.hIndex(arr)
+
+        print(result)
+        print("~")
+
+# } Driver Code Ends
