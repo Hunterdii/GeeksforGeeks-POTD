cd144db95123e23fd44f1eb98efae23489f0e58d
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-09 07:23:14 +0530

    Create December-09.py

---
+++ December 2024 GFG SOLUTION/December-09.py
@@ -0,0 +1,40 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        result = []
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+        result.append(newInterval)
+        return result
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        intervals = [list(map(int, input().split())) for i in range(N)]
+        newEvent = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.insertInterval(intervals, newEvent)
+        print('[', end = '')
+        for i in range(len(res)):
+            print('[', end = '')
+            print(str(res[i][0])+','+str(res[i][1]), end = '')
+            print(']', end = '')
+            if i < len(res)-1:
+                print(',', end='')
+        print(']')
+        print("~")
+# } Driver Code Ends
