0a37625be1f21d45d5b252010d0b71b20a7c9454
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-09 07:23:37 +0530

    Create December-09.java

---
+++ December 2024 GFG SOLUTION/December-09.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[][] = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                a[i][0] = sc.nextInt();
+                a[i][1] = sc.nextInt();
+            }
+            int h[] = new int[2];
+            h[0] = sc.nextInt();
+            h[1] = sc.nextInt();
+            sc.nextLine();
+            Solution obj = new Solution();
+            ArrayList<int[]> ans = obj.insertInterval(a, h);
+            System.out.print("[");
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print("[");
+                System.out.print(ans.get(i)[0] + "," + ans.get(i)[1]);
+                System.out.print("]");
+                if (i != ans.size() - 1) System.out.print(",");
+            }
+            System.out.println("]");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0; int n = intervals.length;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result;
+    }
+}
