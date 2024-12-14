4256fea4b1870eb15ef069d70c3b146464e3c5e6
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-14 18:21:09 +0530

    Create December-14.cpp

---
+++ December 2024 GFG SOLUTION/December-14.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int search(vector<int>& arr, int key) {
+        int n = arr.size();
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
