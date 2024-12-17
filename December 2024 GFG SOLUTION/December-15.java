4d52afef9ae3056db364b6feafa58db335b7f9d5
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-17 08:43:48 +0530

    Create December-15.java

---
+++ December 2024 GFG SOLUTION/December-15.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            int idx = obj.peakElement(a);
+            int n = a.length;
+            if (idx < 0 && idx >= n)
+                System.out.println("false");
+            else {
+                if (n == 1 && idx == 0)
+                    f = 1;
+                else if (idx == 0 && a[0] > a[1])
+                    f = 1;
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
+                    f = 1;
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
+                    f = 1;
+                else
+                    f = 0;
+                if (f == 1) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    public int peakElement(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low; 
+    }
+}
