c3ea10469b0c1c74ea7a4398df4ebe9b07b290c8
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-17 08:44:18 +0530

    Create December-15.py

---
+++ December 2024 GFG SOLUTION/December-15.py
@@ -0,0 +1,49 @@
+
+class Solution:
+    def peakElement(self, arr):
+        low, high = 0, len(arr) - 1
+
+        while low < high:
+            mid = low + (high - low) // 2
+            if arr[mid] > arr[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
+        
+        return low  
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        arr = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
+
+        index = Solution().peakElement(arr)
+        n = len(arr)
+        flag = False
+        if index < 0 or index >= n:
+            flag = False
+        else:
+            if index == 0 and n == 1:
+                flag = True
+            elif index == 0 and arr[index] > arr[index + 1]:
+                flag = True
+            elif index == n - 1 and arr[index] > arr[index - 1]:
+                flag = True
+            elif index > 0 and index < n - 1 and arr[
+                    index - 1] < arr[index] and arr[index] > arr[index + 1]:
+                flag = True
+            else:
+                flag = False
+
+        if flag:
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
