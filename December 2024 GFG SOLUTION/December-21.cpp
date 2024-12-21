5d4e4b46b834694079a21109734a5db828f790bf
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-21 11:12:07 +0530

    Create December-21.cpp

---
+++ December 2024 GFG SOLUTION/December-21.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    void rotateby90(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        reverse(mat.begin(), mat.end());
+    }
+};
+
+2)
+class Solution {
+public:
+    void rotateby90(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            for (int i = 0, k = n - 1; i < k; i++, k--) {
+                swap(mat[i][j], mat[k][j]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(n, 0);
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
