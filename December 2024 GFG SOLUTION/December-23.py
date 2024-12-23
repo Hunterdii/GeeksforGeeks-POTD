10c2930a4227a1b8174644ac81a7c6e271cee2f4
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-23 12:43:39 +0530

    Create December-23.py

---
+++ December 2024 GFG SOLUTION/December-23.py
@@ -0,0 +1,42 @@
+
+#User function Template for python3
+
+class Solution:
+    def searchRowMatrix(self, mat, x):
+        for row in mat:
+            if x in row: 
+                return True
+        return False
+    	
+
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+        x = int(data[index])
+        index += 1
+        ob = Solution()
+        if ob.searchRowMatrix(matrix, x):
+            print("true")
+        else:
+            print("false")
+        print("~")
+# } Driver Code Ends
