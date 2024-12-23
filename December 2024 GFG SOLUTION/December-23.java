4b2d2f65134510215d96a176beecaee53d8f092a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-23 12:43:19 +0530

    Create December-23.java

---
+++ December 2024 GFG SOLUTION/December-23.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+
+            int matrix[][] = new int[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            int target = sc.nextInt();
+
+            Solution x = new Solution();
+
+            if (x.searchRowMatrix(matrix, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        for (int[] row : mat) {
+            if (Arrays.binarySearch(row, x) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
