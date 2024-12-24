cbb6759dc37a9da91f3d25156438c153312f9ded
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-24 10:24:15 +0530

    Create December-24.cpp

---
+++ December 2024 GFG SOLUTION/December-24.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int x) {
+        int n = mat.size(), m = mat[0].size(), l = 0, r = n * m - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2, val = mat[mid / m][mid % m];
+            if (val == x) return true;
+            val < x ? l = mid + 1 : r = mid - 1;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
