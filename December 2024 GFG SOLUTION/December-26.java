22eceb6fa2fbab192b9f0b27590f50701369a1ca
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-26 11:33:40 +0530

    Create December-26.java

---
+++ December 2024 GFG SOLUTION/December-26.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int[] arr, int target) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : arr) {
+            if (seen.contains(target - num)) return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
