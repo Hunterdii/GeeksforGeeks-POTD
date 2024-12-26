9124c381c172d57fae1e662ed19f1deefce256b3
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-26 11:34:01 +0530

    Create December-26.py

---
+++ December 2024 GFG SOLUTION/December-26.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+class Solution:
+    def twoSum(self, arr, target):
+        seen = set()
+        for num in arr:
+            if target - num in seen:
+                return True
+            seen.add(num)
+        return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+
+
+def main():
+    T = int(input())
+    while T > 0:
+        x = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.twoSum(arr, x)
+        if ans:
+            print("true")
+        else:
+            print("false")
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
