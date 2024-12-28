ade4c2ace6af67047decaaaeb7ef52ce73ecaa66
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-28 09:54:22 +0530

    Create December-28.java

---
+++ December 2024 GFG SOLUTION/December-28.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        res.add(Arrays.asList(i, j, k));
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            List<List<Integer>> res = obj.findTriplets(nums);
+            Collections.sort(res, (a, b) -> {
+                for (int i = 0; i < a.size(); i++) {
+                    if (!a.get(i).equals(b.get(i))) {
+                        return a.get(i) - b.get(i);
+                    }
+                }
+                return 0;
+            });
+            if (res.size() == 0) {
+                System.out.println("[]");
+            }
+            for (int i = 0; i < res.size(); i++) {
+                for (int j = 0; j < res.get(i).size(); j++) {
+                    System.out.print(res.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
