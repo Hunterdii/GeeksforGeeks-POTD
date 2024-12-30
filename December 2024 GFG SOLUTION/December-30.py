47ec5f4b6a0497cd2042b793d5d63bad37167755
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-30 07:19:12 +0530

    Create December-30.py

---
+++ December 2024 GFG SOLUTION/December-30.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:    
+    def findUnion(self, a, b):
+        return len(set(a).union(b))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob = Solution()
+
+        print(ob.findUnion(a, b))
+        print("~")
+
+# } Driver Code Ends
