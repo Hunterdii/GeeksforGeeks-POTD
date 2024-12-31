47462d040cdbde5a2cf684124feb85748d43297f
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2024-12-31 08:32:59 +0530

    Create December-31.cpp

---
+++ December 2024 GFG SOLUTION/December-31.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.count(num - 1)) {
+                int current = num, count = 1;
+                while (numSet.count(current + 1)) current++, count++;
+                longest = max(longest, count);
+            }
+        }
+        return longest;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.longestConsecutive(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
