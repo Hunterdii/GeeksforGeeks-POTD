353f49d7aa68406ef16932bfd3dcacb3dfde262a
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-01-05 13:42:20 +0530

    Create January-05.java

---
+++ January 2025 GFG SOLUTION/January-05.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    int countPairs(int[] arr, int target) {
+        Arrays.sort(arr);
+        int l = 0, r = arr.length - 1, ans = 0;
+        while (l < r) if (arr[l] + arr[r] < target) ans += (r - l++);
+        else r--;
+        return ans;
+    }
+}
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine());
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
