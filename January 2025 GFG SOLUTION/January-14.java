7cb7138d60137573230c0e340efa470ded640642
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-01-14 14:05:01 +0530

    Create January-14.java

---
+++ January 2025 GFG SOLUTION/January-14.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public static int findEquilibrium(int[] arr) {
+        long sum = Arrays.stream(arr).asLongStream().sum(), sum2 = 0;
+        for (int i = 0; i < arr.length; sum2 += arr[i++])
+            if ((sum -= arr[i]) == sum2) return i;
+        return -1;
+    }
+}
