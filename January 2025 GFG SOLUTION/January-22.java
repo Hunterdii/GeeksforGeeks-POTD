95bbd2bda6556fe2ce44f3a3967e51d46c9d71a3
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-01-22 11:57:29 +0530

    Create January-22.java

---
+++ January 2025 GFG SOLUTION/January-22.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+// driver
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// solution
+
+class Solution {
+    Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    Node addTwoLists(Node l1, Node l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.data;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.data;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            tail.next = new Node(sum % 10);
+            tail = tail.next;
+        }
+
+        Node res = reverse(dummy.next);
+        while (res != null && res.data == 0 && res.next != null) {
+            res = res.next;
+        }
+        return res;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GfG {
+
+    static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(read.readLine());
+        while (T-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            Node num1 = new Node(Integer.parseInt(str[0]));
+            Node tail = num1;
+            for (int i = 1; i < n; i++) {
+                int val = Integer.parseInt(str[i]);
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+
+            String str2[] = read.readLine().trim().split(" ");
+            int m = str2.length;
+
+            Node num2 = new Node(Integer.parseInt(str2[0]));
+            tail = num2;
+            for (int i = 1; i < m; i++) {
+
+                tail.next = new Node(Integer.parseInt(str2[i]));
+                tail = tail.next;
+            }
+
+            Solution g = new Solution();
+            Node res = g.addTwoLists(num1, num2);
+            printList(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
