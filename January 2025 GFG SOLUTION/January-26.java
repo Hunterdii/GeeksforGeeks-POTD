b6cc96b1d6641ebebd7f16b5fd36d7b1442e7c40
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-01-26 14:32:41 +0530

    Create January-26.java

---
+++ January 2025 GFG SOLUTION/January-26.java
@@ -0,0 +1,130 @@
+//{ Driver Code Starts
+// driver code
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+}
+
+class GFG {
+    public static Node newNode(int data) {
+        Node temp = new Node();
+        temp.data = data;
+        temp.next = null;
+        return temp;
+    }
+
+    public static void makeLoop(Node head, int x) {
+        if (x == 0) return;
+        Node curr = head;
+        Node last = head;
+
+        int currentPosition = 1;
+        while (currentPosition < x) {
+            curr = curr.next;
+            currentPosition++;
+        }
+
+        while (last.next != null) last = last.next;
+        last.next = curr;
+    }
+
+    public static boolean detectLoop(Node head) {
+        Node hare = head.next;
+        Node tortoise = head;
+        while (hare != tortoise) {
+            if (hare == null || hare.next == null) return false;
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+        }
+        return true;
+    }
+
+    public static int length(Node head) {
+        int ret = 0;
+        while (head != null) {
+            ret += 1;
+            head = head.next;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            int num = Integer.parseInt(str[0]);
+            Node head = newNode(num);
+            Node tail = head;
+
+            for (int i = 1; i < n; i++) {
+                num = Integer.parseInt(str[i]);
+                tail.next = newNode(num);
+                tail = tail.next;
+            }
+
+            int pos = Integer.parseInt(read.readLine());
+            makeLoop(head, pos);
+
+            Solution x = new Solution();
+            x.removeLoop(head);
+
+            if (detectLoop(head) || length(head) != n)
+                System.out.println("false");
+            else
+                System.out.println("true");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+}
+*/
+
+class Solution {
+    public static void removeLoop(Node head) {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null && (slow = slow.next) != (fast = fast.next.next));
+        if (fast == null || fast.next == null) return;
+        for (slow = head; slow != fast; slow = slow.next, fast = fast.next);
+        while (fast.next != slow) fast = fast.next;
+        fast.next = null;
+    }
+}
+
+
+class Solution {
+    public static void removeLoop(Node head) {
+        HashSet<Node> visited = new HashSet<>();
+        Node prev = null;
+
+        while (head != null) {
+            // If the node is already visited, break the loop
+            if (visited.contains(head)) {
+                prev.next = null;
+                break;
+            }
+            visited.add(head);  // Mark the current node as visited
+            prev = head;
+            head = head.next;
+        }
+    }
+}
