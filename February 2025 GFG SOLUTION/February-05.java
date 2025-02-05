6214e518b13230bdc477ff23044ebb0df06a5b12
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-05 11:22:36 +0530

    Create February-05.java

---
+++ February 2025 GFG SOLUTION/February-05.java
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Contributed by Sudarshan Sharma
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static String levelOrder(Node root) {
+        if (root == null) return "N\n";
+
+        StringBuilder str = new StringBuilder();
+        Queue<Node> qq = new LinkedList<>();
+        qq.add(root);
+
+        while (!qq.isEmpty()) {
+            Node curr = qq.poll();
+
+            if (curr == null) {
+                str.append("N ");
+                continue;
+            }
+            str.append(curr.data).append(" ");
+            qq.add(curr.left);
+            qq.add(curr.right);
+        }
+
+        // Trim trailing non-digit characters
+        while (str.length() > 0 && !Character.isDigit(str.charAt(str.length() - 1))) {
+            str.deleteCharAt(str.length() - 1);
+        }
+
+        return str.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution g = new Solution();
+            g.mirror(root);
+            System.out.println(levelOrder(root));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+// class Solution {
+//     void mirror(Node node) {
+//         if(node==null)return;
+//         mirror(node.left);
+//         mirror(node.right);
+//         Node t=node.left; node.left=node.right; node.right=t;
+//     }
+// }
+
+
+class Solution {
+    void mirror(Node node) {
+        if (node == null) return;
+        mirror(node.left);
+        mirror(node.right);
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
