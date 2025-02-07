6ab82da4a3a9381b7cb67c175642c0434edc5f97
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-07 13:46:22 +0530

    Create February-07.py

---
+++ February 2025 GFG SOLUTION/February-07.py
@@ -0,0 +1,124 @@
+
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def inOrder(self, root):
+        a = []
+        def f(r):
+            if r:
+                f(r.left)
+                a.append(r.data)
+                f(r.right)
+        f(root)
+        return a
+
+2)
+class Solution:
+    def inOrder(self, root):
+        result, stack = [], []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            result.append(curr.data)
+            curr = curr.right
+        return result
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Contributed by Sudarshan Sharma
+from collections import deque
+
+
+# Tree Node
+class Node:
+
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    #Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        res = Solution().inOrder(root)
+        for i in range(len(res)):
+            print(res[i], end=" ")
+        print()
+
+        print("~")
+
+# } Driver Code Ends
