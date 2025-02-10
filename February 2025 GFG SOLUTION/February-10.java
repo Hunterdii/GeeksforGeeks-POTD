870055d79ed85dd4a85b489fc1808ee93ddd0a0d
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-10 08:28:31 +0530

    Create February-10.java

---
+++ February 2025 GFG SOLUTION/February-10.java
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the current node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+
+            System.out.println(ob.sumK(root, k));
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Integer,Integer> m = new HashMap<>();
+    int cnt = 0;
+    void dfs(Node r, int k, int s) {
+        if(r==null)return;
+        s += r.data;
+        if(m.containsKey(s-k)) cnt += m.get(s-k);
+        m.put(s, m.getOrDefault(s,0)+1);
+        dfs(r.left, k, s);
+        dfs(r.right, k, s);
+        m.put(s, m.get(s)-1);
+        if(m.get(s)==0)m.remove(s);
+    }
+    public int sumK(Node r, int k) {
+        m.put(0,1);
+        dfs(r, k, 0);
+        return cnt;
+    }
+}
