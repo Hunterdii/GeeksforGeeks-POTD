a34ebd2aedea55a2ee83155fc8a7161546bbdd84
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-16 11:04:49 +0530

    Create February-16.java

---
+++ February 2025 GFG SOLUTION/February-16.java
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void deletetree(Node root) {
+        if (root == null) return;
+        deletetree(root.left);
+        deletetree(root.right);
+        root.left = null;
+        root.right = null;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Tree tr = new Tree();
+            ArrayList<Integer> A = tr.serialize(root);
+            deletetree(root);
+            root = null;
+
+            Node getRoot = tr.deSerialize(A);
+            printInorder(getRoot);
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the given function
+Node is as follows:
+class Tree{
+    int data;
+    Tree left,right;
+    Tree(int d){
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class Tree {
+    public ArrayList<Integer> serialize(Node r) {
+        ArrayList<Integer> a = new ArrayList<>();
+        s(r, a);
+        return a;
+    }
+    void s(Node r, ArrayList<Integer> a) {
+        if(r==null){a.add(-1); return;}
+        a.add(r.data);
+        s(r.left, a);
+        s(r.right, a);
+    }
+    public Node deSerialize(ArrayList<Integer> a) {
+        int[] i = {0};
+        return d(a, i);
+    }
+    Node d(ArrayList<Integer> a, int[] i) {
+        if(i[0]>=a.size() || a.get(i[0])==-1){i[0]++; return null;}
+        Node r = new Node(a.get(i[0]++));
+        r.left = d(a, i);
+        r.right = d(a, i);
+        return r;
+    }
+}
