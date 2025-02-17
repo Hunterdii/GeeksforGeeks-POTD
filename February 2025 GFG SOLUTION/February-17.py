bd01eba7d7d51495727c4a7894e44c3a748debee
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-17 11:13:01 +0530

    Create February-17.py

---
+++ February 2025 GFG SOLUTION/February-17.py
@@ -0,0 +1,21 @@
+class Solution:
+    def kLargest(self, arr, k):
+        return sorted(arr, reverse=True)[:k]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        k = int(input().strip())
+        ob = Solution()
+        ans = ob.kLargest(arr, k)
+
+        print(" ".join(map(str, ans)))
+        tc -= 1
+        print("~")
+
+# } Driver Code Ends
