9496b72dfd02d59a2850a148c37416597e7fbfde
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-17 11:12:39 +0530

    Create February-17.java

---
+++ February 2025 GFG SOLUTION/February-17.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public ArrayList<Integer> kLargest(int[] arr, int k) {
+        Arrays.sort(arr);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = arr.length - 1; i >= arr.length - k; i--) res.add(arr[i]);
+        return res;
+    }
+}
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.kLargest(arr, k);
+
+            // Print the result as a space-separated string
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println(); // New line after printing the results
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
