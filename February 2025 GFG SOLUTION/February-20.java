3970c5802c3dbdf94e51ac95a1a9b6be99f0a28c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-20 14:58:14 +0530

    Create February-20.java

---
+++ February 2025 GFG SOLUTION/February-20.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            int[] nums = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                nums[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            ArrayList<Double> ans = ob.getMedian(nums);
+            for (double i : ans) {
+                System.out.printf("%.1f ", i);
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public ArrayList<Double> getMedian(int[] arr) {
+        PriorityQueue<Integer> maxH = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minH = new PriorityQueue<>();
+        ArrayList<Double> res = new ArrayList<>();
+
+        for (int n : arr) {
+            maxH.add(n);
+            minH.add(maxH.poll());
+            if (maxH.size() < minH.size()) maxH.add(minH.poll());
+            res.add(maxH.size() > minH.size() ? (double) maxH.peek() : (maxH.peek() + minH.peek()) / 2.0);
+        }
+        return res;
+    }
+}
