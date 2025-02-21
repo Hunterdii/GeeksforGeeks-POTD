bf37fe170b4c117a6cd7670f603850db6802cd5c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-21 15:08:36 +0530

    Create  February-21.java

---
+++ February 2025 GFG SOLUTION/February-21.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
+            String st = sc.next();
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static boolean isBalanced(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) 
+            if ("({[".indexOf(c) >= 0) st.push(c);
+            else if (st.isEmpty() || Math.abs(st.peek() - c) > 2) return false;
+            else st.pop();
+        return st.isEmpty();
+    }
+}
