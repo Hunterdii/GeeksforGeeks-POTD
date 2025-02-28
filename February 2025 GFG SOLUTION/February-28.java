5f3fa5f4cdd662b4f5dd1c23a5af5406098e0c28
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-02-28 12:22:37 +0530

    Create February-28.java

---
+++ February 2025 GFG SOLUTION/February-28.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public int evaluate(String[] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (String s : arr) {
+            if ("+-*/".contains(s)) {
+                int b = st.pop(), a = st.pop();
+                if (s.equals("+")) st.push(a + b);
+                else if (s.equals("-")) st.push(a - b);
+                else if (s.equals("*")) st.push(a * b);
+                else st.push(a / b);
+            } else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+        return st.peek();
+    }
+}
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int t = Integer.parseInt(line);
+        while (t-- > 0) {
+            line = reader.readLine();
+            String[] arr = line.split(" ");
+            Solution solution = new Solution();
+            System.out.println(solution.evaluate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
