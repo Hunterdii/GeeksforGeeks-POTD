a56b34354385540556b2ca44fcb8b452b6f8b080
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-02 11:59:44 +0530

    Create March-02.py

---
+++ March 2025 GFG SOLUTION/March-02.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        res, dq = [], deque()
+        for i in range(len(arr)):
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+            while dq and arr[dq[-1]] < arr[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                res.append(arr[dq[0]])
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        arr = list(map(int, input().strip().split()))
+        k = int(input())
+        ob = Solution()
+        res = ob.maxOfSubarrays(arr, k)
+        for i in range(len(res)):
+            print(res[i], end=" ")
+        print()
+        print("~")
+# } Driver Code Ends
