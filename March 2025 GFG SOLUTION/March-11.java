c8f8edccc69ca36106dd5e93055233925fdda687
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-11 12:24:12 +0530

    Create March-11.java

---
+++ March 2025 GFG SOLUTION/March-11.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // taking count of stairs
+            int m = sc.nextInt();
+
+            // creating an object of class DynamicProgramming
+            Solution obj = new Solution();
+
+            // calling method countWays() of class
+            // DynamicProgramming
+            System.out.println(obj.countWays(m));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public long countWays(int n) {
+        long a = 1, b = 1;
+        while (n-- > 1) {
+            b += a;
+            a = b - a;
+        }
+        return b;
+    }
+}
