08968a8c1100d4732d9fdaff15152009adbc1f02
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-12 13:44:28 +0530

    Create March-12.java

---
+++ March 2025 GFG SOLUTION/March-12.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = str.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            int res = sln.minCostClimbingStairs(arr);
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int a = cost[0], b = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            int temp = b;
+            b = cost[i] + Math.min(a, b);
+            a = temp;
+        }
+        return Math.min(a, b);
+    }
+}
