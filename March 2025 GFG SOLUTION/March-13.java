c2297511693d936f9c631f538fb3ce74704c9fd2
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-13 11:03:40 +0530

    Create March-13.java

---
+++ March 2025 GFG SOLUTION/March-13.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine());
+
+        while (testCases-- > 0) {
+            int capacity = Integer.parseInt(br.readLine());
+            String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
+
+            int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
+
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
+            }
+
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
+            }
+
+            System.out.println(Solution.knapsack(capacity, val, wt));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    static int knapsack(int W, int[] val, int[] wt) {
+        int[] dp = new int[W + 1];
+        for (int i = 0; i < wt.length; i++) 
+            for (int j = W; j >= wt[i]; j--) 
+                dp[j] = Math.max(dp[j], val[i] + dp[j - wt[i]]);
+        return dp[W];
+    }
+}
