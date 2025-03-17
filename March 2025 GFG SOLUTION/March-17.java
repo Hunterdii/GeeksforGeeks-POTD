fd6e82382dc1a008e15b797829ee3adc6a640da2
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-17 13:30:17 +0530

    Create March-17.java

---
+++ March 2025 GFG SOLUTION/March-17.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean isSubsetSum(int[] arr, int sum) {
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int num : arr)
+            for (int j = sum; j >= num; --j)
+                dp[j] |= dp[j - num];
+        return dp[sum];
+    }
+}
