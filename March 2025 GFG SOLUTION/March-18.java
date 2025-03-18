fae50c87e1773b36dd94603507cfc6a664d23e0c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-18 14:34:14 +0530

    Create March-18.java

---
+++ March 2025 GFG SOLUTION/March-18.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+
+            if (ob.equalPartition(arr))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean equalPartition(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : arr)
+            for (int j = target; j >= num; --j)
+                dp[j] |= dp[j - num];
+        return dp[target];
+    }
+}
