ab0eadf7ce5221cd525cd5d413b422a948472583
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-22 15:00:07 +0530

    Create March-22.java

---
+++ March 2025 GFG SOLUTION/March-22.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int n = s.length;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int[] arr = IntArray.input(br);
+
+            Solution obj = new Solution();
+            int res = obj.maxValue(arr);
+
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int maxValue(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+
+        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
+    }
+
+    private int rob(int[] nums, int l, int r) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = l; i <= r; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
