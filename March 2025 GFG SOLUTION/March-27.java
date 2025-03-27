119048b81c55ae706cc6146fa43c6f884e5f1d84
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-27 13:35:48 +0530

    Create March-27.java

---
+++ March 2025 GFG SOLUTION/March-27.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input (arr)
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] =
+                    Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input (dep)
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] dep = new int[m];
+            for (int i = 0; i < m; i++) {
+                dep[i] =
+                    Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.findPlatform(arr, dep));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int findPlatform(int arr[], int dep[]) {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i = 0, j = 0, cnt = 0, ans = 0, n = arr.length;
+        while(i < n && j < n)
+            if(arr[i] <= dep[j]) { cnt++; ans = Math.max(ans, cnt); i++; }
+            else { cnt--; j++; }
+        return ans;
+    }
+}
