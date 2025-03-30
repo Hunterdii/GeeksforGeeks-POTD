85c3d672e9cc11e5c0d3e29cd46093ab90716f66
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-30 11:54:40 +0530

    Create March-30.java

---
+++ March 2025 GFG SOLUTION/March-30.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            // ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] gas = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) gas[idx++] = i;
+
+            int[] cost = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) cost[idx++] = i;
+
+            int ans = new Solution().startStation(gas, cost);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        int total = 0, curr = 0, start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            curr += diff;
+            if (curr < 0) { start = i + 1; curr = 0; }
+        }
+        return total >= 0 ? start : -1;
+    }
+}
