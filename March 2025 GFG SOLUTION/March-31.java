515d921b3457ee3196aaa9e66d59a96f050f6c98
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-03-31 12:23:18 +0530

    Create March-31.java

---
+++ March 2025 GFG SOLUTION/March-31.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.maxPartitions(s);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int maxPartitions(String s) {
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) 
+            last[s.charAt(i) - 'a'] = i;
+        int count = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end)
+                count++;
+        }
+        return count;
+    }
+}
