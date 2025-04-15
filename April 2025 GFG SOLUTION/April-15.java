e9e8e6c764554a38c49b6e5b14ee6a054e63868e
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-04-15 12:47:42 +0530

    Create April-15.java

---
+++ April 2025 GFG SOLUTION/April-15.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+
+            int V = Integer.parseInt(read.readLine().trim());
+            int E = Integer.parseInt(read.readLine().trim());
+
+            int[][] edges = new int[E][3];
+
+            for (int i = 0; i < E; i++) {
+                String[] parts = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(parts[0]);
+                int v = Integer.parseInt(parts[1]);
+                int w = Integer.parseInt(parts[2]);
+
+                edges[i][0] = u;
+                edges[i][1] = v;
+                edges[i][2] = w;
+            }
+
+            // Read source vertex
+            int S = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            int[] ptr = ob.bellmanFord(V, edges, S);
+
+            // Print the result
+            for (int i = 0; i < ptr.length; i++) {
+                System.out.print(ptr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int)1e8);
+        dist[src] = 0;
+        for (int i = 0; i < V - 1; i++)
+            for (int[] e : edges)
+                if (dist[e[0]] < 1e8 && dist[e[0]] + e[2] < dist[e[1]])
+                    dist[e[1]] = dist[e[0]] + e[2];
+        for (int[] e : edges)
+            if (dist[e[0]] < 1e8 && dist[e[0]] + e[2] < dist[e[1]])
+                return new int[]{-1};
+        return dist;
+    }
+}
