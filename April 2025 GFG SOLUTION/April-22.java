ae57fe03b9ac24ecae6167caf04a28cf009b5148
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-04-22 14:43:52 +0530

    Create April-22.java

---
+++ April 2025 GFG SOLUTION/April-22.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.findUnique(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int findUnique(int[] arr) {
+        int r = 0;
+        for (int x : arr) r ^= x;
+        return r;
+    }
+}
