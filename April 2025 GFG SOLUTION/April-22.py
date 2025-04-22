fa7b86924ae59b76184565fae04c2bdc8df8814e
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-04-22 14:44:17 +0530

    Create April-22.py

---
+++ April 2025 GFG SOLUTION/April-22.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findUnique(self, arr):
+        r = 0
+        for x in arr: r ^= x
+        return r
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.findUnique(arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
