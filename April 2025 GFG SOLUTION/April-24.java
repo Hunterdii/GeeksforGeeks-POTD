dcd3f0d2180a97a6ca8907e25b1772e76e06f9f7
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-04-24 15:54:58 +0530

    Create April-24.java

---
+++ April 2025 GFG SOLUTION/April-24.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSingle(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int getSingle(int[] arr) {
+        int ones = 0, twos = 0;
+        for (int num : arr) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones;
+    }
+}
