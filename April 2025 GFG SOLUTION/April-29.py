da478990cc3090d2ad1aaaa2cfb7dee8e85f3b51
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-04-29 15:20:46 +0530

    Create April-29.py

---
+++ April 2025 GFG SOLUTION/April-29.py
@@ -0,0 +1,61 @@
+'''
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}'''
+	
+class Solution:
+    def segregate(self, head):
+        count = [0, 0, 0]
+        curr = head
+        while curr:
+            count[curr.data] += 1
+            curr = curr.next
+        curr = head
+        for i in range(3):
+            while count[i]:
+                curr.data = i
+                curr = curr.next
+                count[i] -= 1
+        return head
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for value in arr[1:]:
+                tail.next = Node(value)
+                tail = tail.next
+
+        printList(Solution().segregate(head))
+        print("~")
+
+# } Driver Code Ends
