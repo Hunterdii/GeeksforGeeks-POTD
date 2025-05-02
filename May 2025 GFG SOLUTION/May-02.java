af1d760ff86bbc5da2f166e2eec52c283cf22ea7
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-05-02 17:56:04 +0530

    Create May-02.java

---
+++ May 2025 GFG SOLUTION/May-02.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int findMaximum(int[] arr) {
+        return Arrays.stream(arr).max().getAsInt();
+    }
+}
+
+2)
+class Solution {
+    public int findMaximum(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < arr[mid + 1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return arr[low];
+    }
+}
