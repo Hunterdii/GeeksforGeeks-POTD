3e574e3a3528fa5f9cf6b50bb6206337e22fb63c
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-05-03 18:06:11 +0530

    Create May-03.java

---
+++ May 2025 GFG SOLUTION/May-03.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+    Node next;
+    int val;
+
+    public Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t;
+        t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+
+            Node head, tail;
+            String s[] = in.readLine().trim().split(" ");
+            int num = Integer.parseInt(s[0]);
+            head = new Node(num);
+            tail = head;
+            for (int i = 1; i < s.length; i++) {
+                num = Integer.parseInt(s[i]);
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node temp = ob.primeList(head);
+            while (temp != null) {
+                out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            out.println();
+            out.println("~");
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+*/
+
+class Solution {
+    Node primeList(Node h) {
+        int m = 0;
+        for (Node p = h; p != null; p = p.next) m = Math.max(m, p.val);
+        boolean[] s = new boolean[2*m+1];
+        Arrays.fill(s, true);
+        s[0] = s[1] = false;
+        for (int i = 2; i*i <= 2*m; i++)
+            if (s[i])
+                for (int j = i*i; j <= 2*m; j += i) s[j] = false;
+        for (Node p = h; p != null; p = p.next) {
+            int x = p.val, d = 0;
+            while (true) {
+                if (x-d > 1 && s[x-d]) { p.val = x-d; break; }
+                if (x+d <= 2*m && s[x+d]) { p.val = x+d; break; }
+                d++;
+            }
+        }
+        return h;
+    }
+}
