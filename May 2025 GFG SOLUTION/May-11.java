f5ed56f7348a13bdfec80490f5b08ba8c884b079
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-05-11 20:05:14 +0530

    Create May-11.java

---
+++ May 2025 GFG SOLUTION/May-11.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public static int kthLargest(int[] arr, int k) {
+        int[] p = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; ++i) p[i + 1] = p[i] + arr[i];
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; ++i)
+            for (int j = i + 1; j <= arr.length; ++j) {
+                q.add(p[j] - p[i]);
+                if (q.size() > k) q.poll();
+            }
+        return q.peek();
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.kthLargest(arr, k);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
