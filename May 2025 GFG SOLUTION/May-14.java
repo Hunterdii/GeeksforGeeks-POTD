461f2ae5e7a0c8ae87d6f1c653107e7563ba9678
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-05-14 19:24:50 +0530

    Create May-14.java

---
+++ May 2025 GFG SOLUTION/May-14.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+
+            System.out.println(ob.countAndSay(n));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder temp = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    temp.append(count).append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            temp.append(count).append(result.charAt(result.length() - 1));
+            result = temp.toString();
+        }
+        return result;
+    }
+}
