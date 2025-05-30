85c5c2d3faf927c4975f43bcdb6e371c1d9eff76
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-05-30 12:35:11 +0530

    Create May-30.java

---
+++ May 2025 GFG SOLUTION/May-30.java
@@ -0,0 +1,39 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    public int findMaxFork(Node root, int k) {
+        if (root == null) return -1;
+        if (root.data == k) return k;
+        if (root.data < k) {
+            int x = findMaxFork(root.right, k);
+            return x == -1 ? root.data : x;
+        }
+        return findMaxFork(root.left, k);
+    }
+}
+
+2)
+class Solution {
+    public int findMaxFork(Node root, int k) {
+        int res = -1;
+        while (root != null) {
+            if (root.data == k) return k;
+            if (root.data < k) {
+                res = root.data;
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        return res;
+    }
+}
