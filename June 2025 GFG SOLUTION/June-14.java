332c01d4e4b9c0ce1f9e658eb8e0af36a0abff9d
Jomon <202098873+itsjomon@users.noreply.github.com>
2025-06-14 04:00:40 +0530

    Create June-14.java

---
+++ June 2025 GFG SOLUTION/June-14.java
@@ -0,0 +1,27 @@
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+class Solution {
+    public boolean isSymmetric(Node root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(Node t1, Node t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.data == t2.data)
+            && isMirror(t1.left, t2.right)
+            && isMirror(t1.right, t2.left);
+    }
+}
