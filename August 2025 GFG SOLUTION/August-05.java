edeef4a1b0b40a1f5e7ca4b6c5a9ac4751018682
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-08-05 13:52:28 +0530

    Create August-05.java

---
+++ August 2025 GFG SOLUTION/August-05.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+            if (Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--))) return false;
+        }
+        return true;
+    }
+}
