f5a609839052c406cf6f9da273bed90ac5b89dc1
Het Patel <124852522+Hunterdii@users.noreply.github.com>
2025-11-12 20:47:23 +0530

    Create November-12.py

---
+++ November 2025 GFG SOLUTION/November-12.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wildCard(self, txt, pat):
+        n, m = len(txt), len(pat)
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        for j in range(1, m + 1):
+            if pat[j - 1] == '*': dp[0][j] = dp[0][j - 1]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if pat[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                elif pat[j - 1] == '?' or txt[i - 1] == pat[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+        return dp[n][m]
