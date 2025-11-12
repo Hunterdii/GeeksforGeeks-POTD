f58c586143b2b4fb02c836c1e9983d29452adf65
kamakshicse1993-blip <kamakshicse1993@gmail.com>
2025-11-12 16:49:47 +0530

    Create November-12.py

---
+++ November 2025 GFG SOLUTION/November-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wildCard(self, txt, pat):
+        n, m = len(txt), len(pat)
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+
+        # Empty pattern matches empty text
+        dp[0][0] = True
+
+        # Handle patterns starting with '*'
+        for j in range(1, m + 1):
+            if pat[j - 1] == '*':
+                dp[0][j] = dp[0][j - 1]
+
+        # Fill DP table
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if pat[j - 1] == txt[i - 1] or pat[j - 1] == '?':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif pat[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                else:
+                    dp[i][j] = False
+
+        return dp[n][m]
